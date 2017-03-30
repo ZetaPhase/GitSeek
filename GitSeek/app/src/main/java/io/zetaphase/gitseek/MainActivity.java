54a6a0f7bea66aaf26308a38742e9de70633cfc6
xeliot <daveho8888@gmail.com>
2017-03-30 10:36:47 -0700

    add git root and initialize widgets in main activity

--- GitSeek/app/src/main/java/io/zetaphase/gitseek/MainActivity.java
+++ GitSeek/app/src/main/java/io/zetaphase/gitseek/MainActivity.java
@@ -2,12 +2,19 @@ package io.zetaphase.gitseek;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button github_search_button;
+    EditText github_username_edittext;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        github_search_button = (Button) findViewById(R.id.github_search_button);
+        github_username_edittext = (EditText) findViewById(R.id.searchGithubUsername);
     }
 }
