0181fd58affb88fc079f623186498e4d99d644f1
xeliot <daveho8888@gmail.com>
2017-03-30 19:04:58 -0700

    finish get request to github api

--- GitSeek/app/src/main/java/io/zetaphase/gitseek/MainActivity.java
+++ GitSeek/app/src/main/java/io/zetaphase/gitseek/MainActivity.java
@@ -1,15 +1,25 @@
 package io.zetaphase.gitseek;
 
+import android.os.Debug;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
 
 public class MainActivity extends AppCompatActivity {
 
     Button github_search_button;
     EditText github_username_edittext;
+    private String json;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,12 +28,28 @@ public class MainActivity extends AppCompatActivity {
         github_search_button = (Button) findViewById(R.id.github_search_button);
         github_username_edittext = (EditText) findViewById(R.id.searchGithubUsername);
         final String username = github_username_edittext.getText().toString();
+        json = "";
         github_search_button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                RequestQueue queue
+                RequestQueue queue = Volley.newRequestQueue(MainActivity.this);
                 String url = "https://api.github.com/users/" + username;
+                StringRequest stringRequest = new StringRequest(Request.Method.GET, url,
+                        new Response.Listener<String>() {
+                            @Override
+                            public void onResponse(String response) {
+                                setJson(response);
+                            }
+                        }, new Response.ErrorListener() {
+                            @Override
+                            public void onErrorResponse(VolleyError error) {
+                                Log.d("ERROR", "that was unsuccessful");
+                            }
+                });
+                queue.add(stringRequest);
             }
         });
     }
+
+    public void setJson(String json){ this.json = json; }
 }
