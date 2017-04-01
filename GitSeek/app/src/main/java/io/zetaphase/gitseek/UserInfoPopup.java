2b6909056dd91bb54a2bbc2322e67a2a14f63645
xeliot <daveho8888@gmail.com>
2017-03-31 21:28:22 -0700

    instantiate widgets within user info popup class

--- GitSeek/app/src/main/java/io/zetaphase/gitseek/UserInfoPopup.java
+++ GitSeek/app/src/main/java/io/zetaphase/gitseek/UserInfoPopup.java
@@ -4,6 +4,7 @@ import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.DisplayMetrics;
+import android.widget.Button;
 import android.widget.TextView;
 
 /**
@@ -31,5 +32,11 @@ public class UserInfoPopup extends Activity {
         String json = intent.getStringExtra("JSON");
 
         TextView name = (TextView) findViewById(R.id.user_info_name);
+        TextView location = (TextView) findViewById(R.id.user_info_location);
+        TextView numRepos = (TextView) findViewById(R.id.user_info_numrepos);
+        TextView numFollowers = (TextView) findViewById(R.id.user_info_num_followers_following);
+        TextView biography = (TextView) findViewById(R.id.user_info_bio);
+        Button viewRepos = (Button) findViewById(R.id.user_info_view_repos_button);
+        Button goToGithub = (Button) findViewById(R.id.user_info_to_github_button);
     }
 }
